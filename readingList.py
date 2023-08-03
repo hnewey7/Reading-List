6afd200d02154e21cd6e05982cc96d44c5a6a0c2
hnewey7 <hnewey7@gmail.com>
2023-08-03 21:23:55 +0100

    Update readingList.py
    Adding initialisation function.

--- readingList.py
+++ readingList.py
@@ -6,16 +6,43 @@ Created on Thu Aug  3 08:13:28 2023
 """
 
 import atexit
+import os
+
+# Initialisation function.
+def init():
+    
+    # Get log file path.
+    folderPath = os.path.dirname(os.path.abspath(__file__))
+    logPath = folderPath + "\\logs.txt"
+    
+    # Check if file exists.
+    if os.path.exists(logPath):
+        
+        # Read file.
+        log = open(logPath,"r")
+        data = log.readlines()     
+        
+        # Get statistics and stack from data.
+        numberRead = data[0]
+        stack = data[1:len(data)-1]
+
+    else:
+        # Create log path.
+        log = open(logPath,"x")
+        
+    return logPath, numberRead, stack
 
 # Function to save data on close.
 def onClose():
     
     return 
 
-# Main Function
+# Main function.
 def main():
     
-    return
+    # Getting values from initialisation.
+    [logPath, numberRead, stack] = init()
+    
 
 main()
 
