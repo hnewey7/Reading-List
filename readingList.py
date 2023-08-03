365d90a507af7fe3464f137dec8436b432fc5380
hnewey7 <hnewey7@gmail.com>
2023-08-03 08:26:47 +0100

    Update readingList.py
    Updating with 'onClose' function to run at exit of terminal to save data.

--- readingList.py
+++ readingList.py
@@ -5,8 +5,19 @@ Created on Thu Aug  3 08:13:28 2023
 @author: hnewey7
 """
 
+import atexit
+
+# Function to save data on close.
+def onClose():
+    
+    return 
+
+# Main Function
 def main():
     
     return
 
 main()
+
+# Defining function to run at exit.
+atexit(onClose)
