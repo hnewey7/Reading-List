3fa529f5a0653d0ccc2af32ab6fbdbf9798b9543
hnewey7 <hnewey7@gmail.com>
2023-08-04 23:31:26 +0100

    Update readingList.py
    Overall layout added to the script.

--- readingList.py
+++ readingList.py
@@ -4,3 +4,28 @@ Created on Thu Aug  3 08:13:28 2023
 
 @author: hnewey7
 """
+
+# Initialisation function.
+def init():
+    # Getting data from csv file.
+    [numberRead, stack] = getData()
+    
+# Main function.
+def main():
+    # Displaying statistics for user.
+    displayStats(numberRead, stack)
+    
+    # Prompt user for command input
+    userCommand = promptInput()
+    
+    # Evaluate user input.
+    [numberRead, stack] = evalInput(userCommand)
+    
+# On close function.
+def onClose():
+    # Saving data.
+    saveData(numberRead, stack)
+    
+init()
+main()
+onClose()
