6f4d7d551d3eed66c3c8c5af70be7f439fe72bd7
hnewey7 <hnewey7@gmail.com>
2023-08-05 07:45:47 +0100

    Update readingList.py
    Adding 'getLog' function to the initialisation, as well as changing the format of the entire script.

--- readingList.py
+++ readingList.py
@@ -5,27 +5,51 @@ Created on Thu Aug  3 08:13:28 2023
 @author: hnewey7
 """
 
+import os
+import pandas
+
+# - - - - - - - - - - - - - - - - - - - - - 
 # Initialisation function.
 def init():
+    # Getting file location.
+    logPath = getLog()
+    
     # Getting data from csv file.
-    [numberRead, stack] = getData()
+    [numberRead, stack] = getData(logPath)
+
+# Get log function
+def getLog():
+    # Getting events log path.
+    folderPath = os.path.dirname(os.path.abspath(__file__))
+    logPath = folderPath + "\\logs.csv"
     
+    return logPath
+
+# Get data function.
+def getData(logPath):
+    
+    
+    
+    return numberRead, stack
+
+# - - - - - - - - - - - - - - - - - - - - - 
 # Main function.
-def main():
-    # Displaying statistics for user.
-    displayStats(numberRead, stack)
-    
-    # Prompt user for command input
-    userCommand = promptInput()
-    
-    # Evaluate user input.
-    [numberRead, stack] = evalInput(userCommand)
-    
+#def main():
+#    # Displaying statistics for user.
+#    displayStats(numberRead, stack)
+#    
+#    # Prompt user for command input
+#    userCommand = promptInput()
+#    
+#    # Evaluate user input.
+#    [numberRead, stack] = evalInput(userCommand)
+#    
+# - - - - - - - - - - - - - - - - - - - - - 
 # On close function.
-def onClose():
-    # Saving data.
-    saveData(numberRead, stack)
-    
+#def onClose():
+#    # Saving data.
+#    saveData(numberRead, stack)
+#    
 init()
-main()
-onClose()
+#main()
+#onClose()
